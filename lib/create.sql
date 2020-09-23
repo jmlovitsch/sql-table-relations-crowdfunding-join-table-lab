3160982f54a5fb084380171d6cffc9ea9e61ac13
john <jmlovitsch@gmail.com>
2020-09-23 15:50:52 -0500

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,21 @@
-CREATE TABLE project (
+CREATE TABLE projects (
     id INTEGER PRIMARY KEY,
     title TEXT,
     category TEXT,
-    funding_goal REAL,
+    funding_goal INTEGER,
     start_date TEXT,
     end_date TEXT
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     name TEXT,
     age INTEGER
 );
 
-CREATE TABLE pledge (
+CREATE TABLE pledges (
     id INTEGER PRIMARY KEY,
     amount INTEGER,
-    project_id INTEGER,
-    user_id INTEGER
+    user_id INTEGER,
+    project_id INTEGER
 );
