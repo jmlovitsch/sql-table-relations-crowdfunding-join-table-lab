b32f8971b42a4601398ee966d23a18e5a90f905a
john <jmlovitsch@gmail.com>
2020-09-23 13:58:00 -0500

    1

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE project (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal REAL,
+    start_date TEXT,
+    end_date TEXT
+);
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pledge (
+    id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    project_id INTEGER,
+    user_id INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO project (title, category, funding_goal, start_date, end_date) VALUES
+("A", "Ac", 1, "JAN", "FEB"),
+("B", "Ac", 12, "JAN", "FEB"),
+("AB", "Ac", 13, "JAN2", "FEB"),
+("Ad", "Ac", 14, "JAN", "FEB4"),
+("Aa", "Ac", 15, "JAN3", "FEB3"),
+("At", "Ac", 16, "JAN", "FEB"),
+("Ag", "Ac", 17, "JAN5", "FEB"),
+("Ah", "Ac", 18, "JAN", "FEB3"),
+("Are", "Ac", 19, "JAN66", "FEB3"),
+("Ae", "Ac", 109, "JAN", "FEB");
